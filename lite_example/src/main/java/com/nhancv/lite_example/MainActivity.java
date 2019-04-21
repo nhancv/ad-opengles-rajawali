ed4049fe1f2db04ab6ec46ea065f5b18833e3e86
Nhan Cao <nhancv92@gmail.com>
2019-04-21 16:58:25 +0700

    first sample

--- lite_example/src/main/java/com/nhancv/lite_example/MainActivity.java
+++ lite_example/src/main/java/com/nhancv/lite_example/MainActivity.java
@@ -1,13 +1,30 @@
 package com.nhancv.lite_example;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+
+import org.rajawali3d.renderer.Renderer;
+import org.rajawali3d.view.ISurface;
+import org.rajawali3d.view.SurfaceView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Renderer myRenderer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final SurfaceView surface = new SurfaceView(this);
+        surface.setFrameRate(60.0);
+        surface.setRenderMode(ISurface.RENDERMODE_WHEN_DIRTY);
+
+        // Add mSurface to your root view
+        addContentView(surface, new ActionBar.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT));
+
+        myRenderer = new MyRenderer(this);
+        surface.setSurfaceRenderer(myRenderer);
     }
 }
---
+++ lite_example/src/main/java/com/nhancv/lite_example/MyRenderer.java
@@ -0,0 +1,73 @@
+package com.nhancv.lite_example;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.MotionEvent;
+
+import org.rajawali3d.lights.DirectionalLight;
+import org.rajawali3d.materials.Material;
+import org.rajawali3d.materials.methods.DiffuseMethod;
+import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.Texture;
+import org.rajawali3d.math.vector.Vector3;
+import org.rajawali3d.primitives.Sphere;
+import org.rajawali3d.renderer.Renderer;
+
+public class MyRenderer extends Renderer {
+
+    private DirectionalLight directionalLight;
+    private Sphere earthSphere;
+
+    public MyRenderer(Context context) {
+        this(context, false);
+        setFrameRate(60);
+    }
+
+    public MyRenderer(Context context, boolean registerForResources) {
+        super(context, registerForResources);
+
+    }
+
+    @Override
+    protected void onRender(long ellapsedRealtime, double deltaTime) {
+        super.onRender(ellapsedRealtime, deltaTime);
+
+        earthSphere.rotate(Vector3.Axis.Y, 1.0);
+    }
+
+    @Override
+    protected void initScene() {
+        directionalLight = new DirectionalLight(1f, .2f, -1.0f);
+        directionalLight.setColor(1.0f, 1.0f, 1.0f);
+        directionalLight.setPower(2);
+        getCurrentScene().addLight(directionalLight);
+
+        Material material = new Material();
+        material.enableLighting(true);
+        material.setDiffuseMethod(new DiffuseMethod.Lambert());
+        material.setColor(0);
+
+        Texture earthTexture = new Texture("Earth", R.drawable.earthtruecolor_nasa_big);
+        try{
+            material.addTexture(earthTexture);
+
+        } catch (ATexture.TextureException error){
+            Log.d("DEBUG", "TEXTURE ERROR");
+        }
+
+        earthSphere = new Sphere(1, 24, 24);
+        earthSphere.setMaterial(material);
+        getCurrentScene().addChild(earthSphere);
+        getCurrentCamera().setZ(4.2f);
+    }
+
+    @Override
+    public void onOffsetsChanged(float xOffset, float yOffset, float xOffsetStep, float yOffsetStep, int xPixelOffset, int yPixelOffset) {
+
+    }
+
+    @Override
+    public void onTouchEvent(MotionEvent event) {
+
+    }
+}
